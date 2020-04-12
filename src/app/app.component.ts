8648a14179d60d6392e9dc29a48354e6693838a3
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2020-04-12 18:29:51 -0400

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,18 @@ export class AppComponent {
   but1State = 0
   routeOnOff = 0
   routeOnOffTxt = 'Off'
+  showMenuTf = false
+  items =["6D6 Greenville","24C Lowell","13C Lakeview"]
 
   // }
   routeOnOffClick(){
     this.routeOnOff =  this.routeOnOff==0 ? 1 : 0 //toggle  0 1
     this.routeOnOffTxt = this.routeOnOffTxt=='On' ? 'Off':'On'
   } 
+  hamburgOnOffClick(){
+    this.showMenuTf = this.showMenuTf==true ? false : true
+     //toggle  true false  not sure i like ? : if/else
+  }
 } // end of export class AppComponent
 
 
