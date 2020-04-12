85043c9827538b3ab1584ce0441cacacdb2f124a
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2020-04-12 08:28:45 -0400

    Add files via upload

---
+++ src/app/map1/map1AgmMarker.ts
@@ -0,0 +1,12 @@
+export class Map1AgmMarker {
+    public lat: number;
+    public lng: number;
+    public label: string;
+    public draggable: string;
+    public iconUrl: string;
+    public locationId: string;
+    public title: string;
+    public magneticVariation: string;
+    public urlLink1: string;
+    public urlLink2: string;
+    }
